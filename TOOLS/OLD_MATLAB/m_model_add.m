91391fa779aac3bf6bb2cfcfcdf1d673800dd290
Andreas Fichtner <andreas.fichtner@erdw.ethz.ch>
2013-10-07 16:28:47 +0200

    Clean up the repository, i.e. deleting old (Matlab) files.
    Implement an improved version of Q_discrete.py for the computation of relaxation times and weights in the visco-elasticity model that consists in a superposition of standard linear solid.

--- TOOLS/OLD_MATLAB/m_model_add.m
+++
@@ -1,9 +0,0 @@
-function model_c=m_model_add(model_a,model_b)
-
-model_c=model_a;
-
-for k=1:model_a.nsubvol
-    
-    model_c.m(k).v=model_a.m(k).v+model_b.m(k).v;
-    
-end
--- TOOLS/Q_discrete.py
+++ TOOLS/Q_discrete.py
@@ -1,139 +1,167 @@
-from mpl_toolkits.basemap import Basemap
+
 import numpy as np
 import matplotlib.pyplot as plt
 import random as rd
 
 """ computation and visualisation of a discrete absorption-band model
-        C_r=1 and rho=1 are assumed
-        tau is computed from the target Q via 1/Q=0.5*tau*pi
+
+    For a given array of target Q values, the code determines the optimal relaxation
+    times and weights. This is done within in specified frequency range.
+
+    C_r=1 and rho=1 are assumed. tau is computed from the target Q via 1/Q=0.5*tau*pi.
 """
 
-#--------------------------------------------------------------------------------------------------------------------------------------
-#- input ----------------------------------------------------------------------------------------------------------------------------
-#--------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#- input ------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 
-#- general setting
+#- array of target Q's
+Q_0=np.array([25.0, 100.0, 500.0, 1000.0])                             
 
-Q=100.0                             #- target Q
+#- minimum and maximum frequencies for optimisation in Hz
+f_min=1.0/100.0                 
+f_max=1.0/10.0                  
 
-f_min=1.0/100.0                 #- minimum frequency [Hz] for discrete-case optimisation
-f_max=1.0/10.0                  #- maximum frequency in [Hz] for discrete-case optimisation
+#- minimum and maximum frequencies for plotting in Hz
+f_min_plot=1.0/200.0       
+f_max_plot=1.0/5.0           
 
-f_min_plot=1.0/1000.0        #- minimum frequency [Hz] for plotting
-f_max_plot=1.0/1.0            #- maximum frequency in [Hz] for plotting
+#- number of relaxation mechanisms
+N=3                         
 
-#- discrete absorption-band model
+#- optimisation parameters (number of iterations, temperature, temperature decrease)
+max_it=30000             
+T=0.2                       
+d=0.9998                     
 
-N=3                         #- number of relaxation mechanisms
-max_it=10000             #- number of iterations in the nonlinear optimisation
-T=0.1                       #- initial temperature
-d=0.9998                     #- cooling factor
-
-#--------------------------------------------------------------------------------------------------------------------------------------
-#- initialisations ----------------------------------------------------------------------------------------------------------------
-#--------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+#- initialisations --------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 
 #- make logarithmic frequency axis
 f=np.logspace(np.log10(f_min),np.log10(f_max),100)
-w=2*np.pi*f
+w=2.0*np.pi*f
 
 f_plot=np.logspace(np.log10(f_min_plot),np.log10(f_max_plot),100)
-w_plot=2*np.pi*f
+w_plot=2.0*np.pi*f_plot
 
 #- compute tau from target Q
-tau=2/(np.pi*Q)
-
-#--------------------------------------------------------------------------------------------------------------------------------------
-#- computations for discrete absorption-band model -------------------------------------------------------------
-#--------------------------------------------------------------------------------------------------------------------------------------
+tau=2.0/(np.pi*Q_0)
 
 #- compute initial relaxation times: logarithmically distributed relaxation times
 tau_min=1.0/f_max
 tau_max=1.0/f_min
-tau_p=np.logspace(np.log10(tau_min),np.log10(tau_max),N)/(2*np.pi)
+tau_s=np.logspace(np.log10(tau_min),np.log10(tau_max),N)/(2*np.pi)
 
 #- compute initial weights
-D_p=1/tau_p
-D_p=D_p/sum(D_p)
+D=1.0/tau_s
+D=D/sum(D)
 
-#- compute initial sum that we later force to pi/2 over the frequency range of interest
-S=0.0
+#- compute initial Q
+chi=0.0
 
-for n in np.arange(N):
-    S=S+D_p[n]*w*tau_p[n]/(1+w**2*tau_p[n]**2)
+for n in np.arange(len(Q_0)):
+    A=1.0
+    B=0.0
+    for p in np.arange(N):
+        A+=tau[n]*(D[p]*w**2*tau_s[p]**2)/(1.0+w**2*tau_s[p]**2)
+        B+=tau[n]*(D[p]*w*tau_s[p])/(1.0+w**2*tau_s[p]**2)
 
-#- compute initial misfit
-chi=sum((S-np.pi/2.0)**2)
+    Q=A/B
+    chi+=sum((Q-Q_0[n])**2/Q_0[n]**2)
+
+#--------------------------------------------------------------------------------------------------
+#- search for optimal parameters ------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 
 #- random search for optimal parameters
-tau_p_test=np.array(np.arange(N),dtype=float)
-D_p_test=np.array(np.arange(N),dtype=float)
+tau_s_test=np.array(np.arange(N),dtype=float)
+D_test=np.array(np.arange(N),dtype=float)
 
 for it in np.arange(max_it):
 
     #- compute perturbed parameters
-    for k in np.arange(N):
-        tau_p_test[k]=tau_p[k]*(1.0+(0.5-rd.random())*T)
-        D_p_test[k]=D_p[k]*(1.0+(0.5-rd.random())*T)
+    for p in np.arange(N):
+        tau_s_test[p]=tau_s[p]*(1.0+(0.5-rd.random())*T)
+        D_test[p]=D[p]*(1.0+(0.5-rd.random())*T)
 
-    #- compute new S
-    S_test=0.0
-    for k in np.arange(N):
-        S_test=S_test+D_p_test[k]*w*tau_p_test[k]/(1+w**2*tau_p_test[k]**2)
+    #- compute test Q
+    chi_test=0.0
 
-    #- compute new misfit and new temperature
-    chi_test=sum((S_test-np.pi/2.0)**2)
+    for n in np.arange(len(Q_0)):
+        A=1.0
+        B=0.0
+        for p in np.arange(N):
+            A+=tau[n]*(D_test[p]*w**2*tau_s_test[p]**2)/(1.0+w**2*tau_s_test[p]**2)
+            B+=tau[n]*(D_test[p]*w*tau_s_test[p])/(1.0+w**2*tau_s_test[p]**2)
+
+        Q_test=A/B
+        chi_test+=sum((Q_test-Q_0[n])**2/Q_0[n]**2)
+
+    #- compute new temperature
     T=T*d
 
     #- check if the tested parameters are better
     if chi_test<chi:
-        D_p[:]=D_p_test[:]
-        tau_p[:]=tau_p_test[:]
+        D[:]=D_test[:]
+        tau_s[:]=tau_s_test[:]
         chi=chi_test
 
-#- compute optimal Q model
+#--------------------------------------------------------------------------------------------------
+#- print weights and relaxation times -------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 
-A=0.0
-B=0.0
+print 'weights:  ', D
+print 'relaxation times:  ', tau_s
+print 'cumulative rms error:  ', np.sqrt(chi/(len(Q)*len(Q_0)))
 
-for n in np.arange(N):
-    A=A+D_p[n]*w_plot**2*tau_p[n]**2/(1+w_plot**2*tau_p[n]**2)
-    B=B+D_p[n]*w_plot*tau_p[n]/(1+w_plot**2*tau_p[n]**2)
+#--------------------------------------------------------------------------------------------------
+#- plot Q and phase velocity as function of frequency ---------------------------------------------
+#--------------------------------------------------------------------------------------------------
 
-A=1+tau*A
-B=tau*B
+for n in np.arange(len(Q_0)):
 
-Q_discrete=A/B
+    #- compute optimal Q model for misfit calculations
+    A=1.0
+    B=0.0
 
-v_discrete=np.sqrt(2*(A**2+B**2)/(A+np.sqrt(A**2+B**2)))
+    for p in np.arange(N):
+        A+=tau[n]*(D[p]*w**2*tau_s[p]**2)/(1.0+w**2*tau_s[p]**2)
+        B+=tau[n]*(D[p]*w*tau_s[p])/(1.0+w**2*tau_s[p]**2)
 
-#--------------------------------------------------------------------------------------------------------------------------------------
-#- print weights and relaxation times ----------------------------------------------------------------------------------
-#--------------------------------------------------------------------------------------------------------------------------------------
+    Q=A/B
+    chi=np.sqrt(sum((Q-Q_0[n])**2/Q_0[n]**2)/len(Q))
 
-print 'weights:', D_p
-print 'relaxation times:', tau_p
+    print 'individual rms error for Q_0='+str(Q_0[n])+':  '+str(chi)
 
-#--------------------------------------------------------------------------------------------------------------------------------------
-#- plot Q and phase velocity as function of frequency -----------------------------------------------------------
-#--------------------------------------------------------------------------------------------------------------------------------------
+    #- compute optimal Q model for plotting
+    A=1.0
+    B=0.0
 
-plt.subplot(121)
-plt.semilogx([f_min,f_min],[0.9/Q,1.1/Q],'r')
-plt.semilogx([f_max,f_max],[0.9/Q,1.1/Q],'r')
-plt.semilogx([f_min_plot,f_max_plot],[1.0/Q,1.0/Q],'b')
-plt.semilogx(f_plot,1/Q_discrete,'k',linewidth=2)
-plt.xlabel('frequency [Hz]')
-plt.ylabel('1/Q')
-plt.title('absorption (1/Q)')
+    for p in np.arange(N):
+        A+=tau[n]*(D[p]*w_plot**2*tau_s[p]**2)/(1.0+w_plot**2*tau_s[p]**2)
+        B+=tau[n]*(D[p]*w_plot*tau_s[p])/(1.0+w_plot**2*tau_s[p]**2)
 
-plt.subplot(122)
-plt.semilogx([f_min,f_min],[0.9,1.1],'r')
-plt.semilogx([f_max,f_max],[0.9,1.1],'r')
-plt.semilogx(f_plot,v_discrete,'k',linewidth=2)
-plt.xlabel('frequency [Hz]')
-plt.ylabel('v')
-plt.title('phase velocity')
+    Q_plot=A/B
+    v_plot=np.sqrt(2*(A**2+B**2)/(A+np.sqrt(A**2+B**2)))
 
-plt.show()
+
+    plt.subplot(121)
+    plt.semilogx([f_min,f_min],[0.9*Q_0[n],1.1*Q_0[n]],'r')
+    plt.semilogx([f_max,f_max],[0.9*Q_0[n],1.1*Q_0[n]],'r')
+    plt.semilogx([f_min_plot,f_max_plot],[1.0*Q_0[n],1.0*Q_0[n]],'b')
+    plt.semilogx(f_plot,Q_plot,'k',linewidth=2)
+    plt.xlabel('frequency [Hz]')
+    plt.ylabel('Q')
+    plt.title('quality factor Q')
+
+    plt.subplot(122)
+    plt.semilogx([f_min,f_min],[0.9,1.1],'r')
+    plt.semilogx([f_max,f_max],[0.9,1.1],'r')
+    plt.semilogx(f_plot,v_plot,'k',linewidth=2)
+    plt.xlabel('frequency [Hz]')
+    plt.ylabel('v')
+    plt.title('phase velocity')
+
+    plt.show()
 
